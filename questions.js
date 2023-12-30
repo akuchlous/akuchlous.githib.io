1af75b548fa34170cfe0872f328b5e0485110f8f
Ankur Kuchlous <akuchlous@gmail.com>
2023-12-30 15:33:00 -0800

    initial version

---
+++ questions.js
@@ -0,0 +1,33 @@
+
+       var qaData = [
+           {
+               title: "Tinyurl",
+               description: "Design Tinyurl",
+               subsections: [
+                   {
+                       "question": "List the requirements for tinyurl",
+                       question_details: "write description in around 100 words",
+                       grading: 'functionality, algorithm, custom url, redirection, TTL, analytics, scalability, availability, security and api'
+                   },
+                   {
+                       "question": "Question 1.2",
+                       question_details: "Description for Question 1.2",
+                       grading: ""
+                   }
+               ]
+           },
+           {
+               title: "Section 2",
+               description: "Description for Section 2",
+               subsections: [
+                   {
+                       "question": "Question 2.1",
+                       question_details: "Description for Question 2.1"
+                   },
+                   {
+                       "question": "Question 2.2",
+                       question_details: "Description for Question 2.2"
+                   }
+               ]
+           }
+       ];

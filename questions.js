83d40a24ce0421fbde064d7cb2958b73889798ae
Ankur Kuchlous <akuchlous@gmail.com>
2023-12-31 23:49:18 -0800

    new uuid

--- questions.js
+++ questions.js
@@ -5,70 +5,70 @@ var qaData =[
     "description": "",
     "subsections": [
       {
-        "uuid": "e7ce35d6-fefb-484d-9f1f-cd83d8545635",
+        "uuid": "905a15f7-df08-4156-9135-4fd3b06f6e1b",
         "index": "0",
         "question": "Why do we need URL shortening",
         "min_words": "50",
         "question_details": "What is tinyurl? Specify the use case and top level basic requirements."
       },
       {
-        "uuid": "3f1df1e7-a969-49b0-85fa-b2086e10fc90",
+        "uuid": "65676a12-dcbf-4c3a-96c4-26ecc95505e8",
         "index": "1",
         "question": "Requirements and Goals of the System for tinyurl system",
         "min_words": "50",
         "question_details": "Can you expland on short links, redirection, expiry, latency, non guessable, security, TTL, API"
       },
       {
-        "uuid": "dbc3533e-1813-49e6-91c6-b27dd9ee5177",
+        "uuid": "1d4cc9ee-a9b7-4b8d-ad15-28565d9cdb58",
         "index": "2",
         "question": "Capacity Estimation and Constraints for tiny url system",
         "min_words": "50",
         "question_details": "Can you estimate the traffic, storage, bandwidth, memory and other numbers"
       },
       {
-        "uuid": "872fa6f7-b5d3-4107-95f2-1d9ad2a33460",
+        "uuid": "dd7e30ef-69b9-419f-9049-d19a4618307e",
         "index": "3",
         "question": " System APIs for tinyurl",
         "min_words": "50",
         "question_details": "What are the apis for the system that you put like to design"
       },
       {
-        "uuid": "04be9f42-5222-4c7d-8ee4-8607a156b481",
+        "uuid": "2392ab9f-5e66-4c8f-821f-19c6710f9146",
         "index": "4",
         "question": "DataBase Design for tinyurl",
         "min_words": "50",
         "question_details": "Can you expland on SQL vs NoSQL, which one can be used and why?"
       },
       {
-        "uuid": "73d8ce73-27f0-46ea-871b-3d737d862f29",
+        "uuid": "80e92f4b-49b8-4cec-b5e0-844ef5fa2855",
         "index": "5",
         "question": "encoding url using base64 for tinyurl keys",
         "min_words": "100",
         "question_details": "How will you do the encoding for tinyurls? Which one to use in counter, hashing, offline genertion of keys or other schemes and pros and cons of each."
       },
       {
-        "uuid": "d921c460-432f-4f2a-b37d-76f6d2fae766",
+        "uuid": "cb4a2d2f-cfa9-4a5f-a052-ef61cc64f5ac",
         "index": "6",
         "question": "Generating keys offline for tinyurl",
         "min_words": "100",
         "question_details": "Explain the details about the offline key generations. What happens to url which are already been shortened?"
       },
       {
-        "uuid": "ffef9017-1ef9-4cc0-9140-60099b91f2ba",
+        "uuid": "adaaea37-2d47-48cf-ae2f-6d4f9c3d2844",
         "index": "7",
         "question": "how will do  Data Partitioning and Replication for tinyurl",
         "min_words": "100",
         "question_details": "How will you manage big data across geogrphic areas? Explain with concepts on paritioning and replication for tinyurl."
       },
       {
-        "uuid": "c382722a-ac76-4899-a728-4c511bf27c4e",
+        "uuid": "f4d7dbdf-7f2a-430d-b139-ee05a78a3c47",
         "index": "8",
         "question": "How will you do caching for tinyurl",
         "min_words": "100",
         "question_details": "How will cachinng work for tinyurl? "
       },
       {
-        "uuid": "90f7707b-2d06-4443-9db9-34ab102a6d65",
+        "uuid": "22e695c7-7a20-4d7a-8ed0-13d816c46f41",
         "index": "9",
         "question": "What statistics for ech tinyurl will you keep",
         "min_words": "100",
